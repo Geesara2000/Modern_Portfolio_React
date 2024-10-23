9eb4c933daf43ed87706a3ed0b3cd0ebd77f73be
Geesara2000 <geesaraimal12@gmail.com>
2024-10-23 11:24:11 +0530

    create projects section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
 import Technologies from "./components/Technologies"
 
 
@@ -17,6 +18,7 @@ const App = () => {
           <Navbar/>
           <Hero />
           <Technologies />
+          <Projects />
       </div>
     </div>
 
